7564091f0c103fd7051311a8899e0c9a1a869d9e
Shadow279 <hhhand123@gmail.com>
2022-11-07 14:21:13 -0800

    Updated File upload

--- frontend/src/components/views/Dashboard.js
+++ frontend/src/components/views/Dashboard.js
@@ -1,110 +1,88 @@
-import React from "react";
-// react-bootstrap components
-import { Container } from "react-bootstrap";
-import { useState, useMemo } from "react";
-import Papa from "papaparse";
-import Pagination from "../Pagination/Pagination";
-import "./style.scss";
-import { Row, Col } from "react-bootstrap";
-
-let PageSize = 10;
+import React, { useState } from "react";
+import * as XLSX from "xlsx";
+import DataTable from "react-data-table-component";
 
 function Dashboard() {
-  const [currentPage, setCurrentPage] = useState(1);
-  // State to store parsed data
-  const [parsedData, setParsedData] = useState([]);
+  const [columns, setColumns] = useState([]);
+  const [data, setData] = useState([]);
 
-  //State to store table Column name
-  const [tableRows, setTableRows] = useState([]);
+  // process CSV data
+  const processData = (dataString) => {
+    const dataStringLines = dataString.split(/\r\n|\n/);
+    const headers = dataStringLines[0].split(
+      /,(?![^"]*"(?:(?:[^"]*"){2})*[^"]*$)/
+    );
 
-  //State to store the values
-  const [values, setValues] = useState([]);
+    const list = [];
+    for (let i = 1; i < dataStringLines.length; i++) {
+      const row = dataStringLines[i].split(
+        /,(?![^"]*"(?:(?:[^"]*"){2})*[^"]*$)/
+      );
+      if (headers && row.length == headers.length) {
+        const obj = {};
+        for (let j = 0; j < headers.length; j++) {
+          let d = row[j];
+          if (d.length > 0) {
+            if (d[0] == '"') d = d.substring(1, d.length - 1);
+            if (d[d.length - 1] == '"') d = d.substring(d.length - 2, 1);
+          }
+          if (headers[j]) {
+            obj[headers[j]] = d;
+          }
+        }
 
-  const currentTableData = useMemo(() => {
-    const firstPageIndex = (currentPage - 1) * PageSize;
-    const lastPageIndex = firstPageIndex + PageSize;
-    return values.slice(firstPageIndex, lastPageIndex);
-  }, [currentPage]);
+        // remove the blank rows
+        if (Object.values(obj).filter((x) => x).length > 0) {
+          list.push(obj);
+        }
+      }
+    }
 
-  const changeHandler = (event) => {
-    // Passing file data (event.target.files[0]) to parse using Papa.parse
-    Papa.parse(event.target.files[0], {
-      header: true,
-      skipEmptyLines: true,
-      complete: function (results) {
-        const rowsArray = [];
-        const valuesArray = [];
+    // prepare columns list from headers
+    const columns = headers.map((c) => ({
+      name: c,
+      selector: c,
+    }));
 
-        // Iterating data to get column name and their values
-        results.data.map((d) => {
-          rowsArray.push(Object.keys(d));
-          valuesArray.push(Object.values(d));
-        });
+    setData(list);
+    setColumns(columns);
+  };
 
-        // Parsed Data Response in array format
-        setParsedData(results.data);
-
-        // Filtered Column Names
-        setTableRows(rowsArray[0]);
-
-        // Filtered Values
-        setValues(valuesArray);
-      },
-    });
+  // handle file upload
+  const handleFileUpload = (e) => {
+    const file = e.target.files[0];
+    console.log("file path  ", file);
+    const reader = new FileReader();
+    reader.onload = (evt) => {
+      /* Parse data */
+      const bstr = evt.target.result;
+      const wb = XLSX.read(bstr, { type: "binary" });
+      /* Get first worksheet */
+      const wsname = wb.SheetNames[0];
+      const ws = wb.Sheets[wsname];
+      /* Convert array of arrays */
+      const data = XLSX.utils.sheet_to_csv(ws, { header: 1 });
+      processData(data);
+    };
+    reader.readAsBinaryString(file);
   };
 
   return (
-    <Container fluid>
-      <div>
-        <Row>
-          <div className="col-2 mt-3">Choose a CSV file to display:</div>
-          <div className="col-6">
-            <input
-              type="file"
-              name="file"
-              onChange={changeHandler}
-              accept=".csv"
-              style={{ display: "block", margin: "10px auto", height: "auto" }}
-            />
-          </div>
-        </Row>
-
-        <br />
-        <br />
-        {/* Table */}
-        <table>
-          <thead>
-            <tr>
-              {tableRows.map((rows, index) => {
-                return (
-                  <th className="font-weight-bold" key={index}>
-                    {rows}
-                  </th>
-                );
-              })}
-            </tr>
-          </thead>
-          <tbody>
-            {currentTableData.map((value, index) => {
-              return (
-                <tr key={index}>
-                  {value.map((val, i) => {
-                    return <td key={i}>{val}</td>;
-                  })}
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-        <Pagination
-          className="pagination-bar"
-          currentPage={currentPage}
-          totalCount={values.length}
-          pageSize={PageSize}
-          onPageChange={(page) => setCurrentPage(page)}
-        />
-      </div>
-    </Container>
+    <div>
+      <input
+        className="h-auto"
+        type="file"
+        accept=".csv,.xlsx,.xls"
+        onChange={handleFileUpload}
+      />
+      <DataTable
+        className="mt-3"
+        pagination
+        highlightOnHover
+        columns={columns}
+        data={data}
+      />
+    </div>
   );
 }
 
