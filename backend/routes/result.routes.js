be22ef15e0452e331e203d51984c933095cd3905
Aditya Baravkar <aditya.baravkar@sjsu.edu>
2023-01-22 15:46:45 -0800

    Removed express app

--- backend/routes/result.routes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const resultController = require("../controllers/result.controller");
-
-router.get("/getResults/:userId", resultController.getResults);
-
-module.exports = router;
